114aada216c00835de99f3d3ed211c3bda36a15e
ResistanceTo <resistanceto@outlook.com>
2025-10-13 16:47:08 +0800

    补充一些开发计划

--- src/config/roadmap.ts
+++ src/config/roadmap.ts
@@ -2,32 +2,6 @@ import { TodoStatus, type TodoNode } from "./types";
 
 export const roadmap: TodoNode[] = [
 	{
-		title: "我的",
-		children: [
-			{
-				title: "个人信息",
-				children: [
-					{
-						title: "资产",
-						status: TodoStatus.Completed,
-					},
-					{
-						title: "粉丝",
-						status: TodoStatus.Completed,
-					},
-					{
-						title: "关注",
-						status: TodoStatus.Bug,
-					}
-				]
-			},
-			{
-				title: "设置",
-				status: TodoStatus.Planned,
-			}
-		],
-	},
-	{
 		title: "首页",
 		children: [
 			{
@@ -42,7 +16,11 @@ export const roadmap: TodoNode[] = [
 						status: TodoStatus.Completed,
 					},
 					{
-						title: "热榜",
+						title: "热门搜索",
+						status: TodoStatus.Planned,
+					},
+					{
+						title: "搜索建议",
 						status: TodoStatus.Planned,
 					}
 				]
@@ -67,19 +45,159 @@ export const roadmap: TodoNode[] = [
 		],
 	},
 	{
+		title: "动态",
+		children: [
+			{
+				title: "视频",
+				status: TodoStatus.Completed,
+			},
+			{
+				title: "综合",
+				status: TodoStatus.Planned,
+			}
+		],
+	},
+	{
+		title: "我的",
+		children: [
+			{
+				title: "个人信息",
+				children: [
+					{
+						title: "基本信息（名字、简介等等）",
+						status: TodoStatus.Completed,
+					},
+					{
+						title: "硬币",
+						status: TodoStatus.Completed,
+					},
+					{
+						title: "粉丝",
+						children: [
+							{
+								title: "数量",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "二级列表",
+								status: TodoStatus.Planned,
+							}
+						]
+					},
+					{
+						title: "关注",
+						children: [
+							{
+								title: "数量",
+								status: TodoStatus.Bug,
+							},
+							{
+								title: "二级列表",
+								status: TodoStatus.Planned,
+							}
+						]
+					},
+					{
+						title: "历史记录",
+						children: [
+							{
+								title: "列表展示",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "基本信息",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "播放",
+								status: TodoStatus.Bug,
+							}
+						]
+					},
+					{
+						title: "收藏夹",
+						children: [
+							{
+								title: "收藏列表展示",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "收藏视频内容展示",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "基本信息",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "播放",
+								status: TodoStatus.Completed,
+							}
+						]
+					},
+					{
+						title: "稍后再看",
+						children: [
+							{
+								title: "列表展示",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "基本信息",
+								status: TodoStatus.Completed,
+							},
+							{
+								title: "播放",
+								status: TodoStatus.Completed,
+							}
+						]
+					}
+				]
+			},
+			{
+				title: "设置",
+				status: TodoStatus.Planned,
+				children: [
+					{
+						title: "隐私",
+						status: TodoStatus.Planned,
+					}
+				]
+			}
+		],
+	},
+	{
 		title: "视频详情页",
 		children: [
 			{
 				title: "视频播放",
-				status: TodoStatus.Completed,
+				children: [
+					{
+						title: "基本播放能力",
+						status: TodoStatus.Completed,
+					},
+					{
+						title: "长按倍速播放",
+						status: TodoStatus.InProgress,
+					},
+				]
 			},
 			{
-				title: "弹幕播放",
-				status: TodoStatus.InProgress,
-			},
-			{
-				title: "弹幕设置",
-				status: TodoStatus.InProgress,
+				title: "弹幕",
+				children: [
+					{
+						title: "弹幕播放",
+						status: TodoStatus.InProgress,
+					},
+					{
+						title: "弹幕设置",
+						status: TodoStatus.InProgress,
+					},
+					{
+						title: "高级弹幕",
+						status: TodoStatus.Planned,
+					},
+				]
 			},
 			{
 				title: "操作",
@@ -100,7 +218,41 @@ export const roadmap: TodoNode[] = [
 						title: "分享",
 						status: TodoStatus.Planned,
 					},
+					{
+						title: "关注",
+						status: TodoStatus.Planned,
+					},
 				]
+			}
+		]
+	},
+	{
+		title: "UP主",
+		children: [
+			{
+				title: "个人信息",
+				status: TodoStatus.Planned,
+			},
+			{
+				title: "关注",
+				status: TodoStatus.Planned,
+			}
+		]
+	},
+	{
+		title: "评论",
+		children: [
+			{
+				title: "基本展示",
+				status: TodoStatus.Planned,
+			},
+			{
+				title: "楼中楼展示",
+				status: TodoStatus.Planned,
+			},
+			{
+				title: "回复",
+				status: TodoStatus.Planned,
 			}
 		]
 	}
