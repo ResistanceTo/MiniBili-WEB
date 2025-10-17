0d28f6305fb51d29cfebff17c6eda65c6c212a7d
ResistanceTo <resistanceto@outlook.com>
2025-10-17 19:53:38 +0800

    添加常见问题

--- src/config/faq.ts
+++ src/config/faq.ts
@@ -5,4 +5,12 @@ export const faq: FAQ[] = [
 		question: "iPad兼容？",
 		answer: "主要面向平台是iPhone，iPad会使用拉伸的方式展示，可能部分内容不那么优雅",
 	},
+	{
+		question: "是否考虑支持iOS17以下的系统",
+		answer: "不会，许多新特性只有iOS17才提供。",
+	},
+	{
+		question: "有建议、问题或bug提到哪里？",
+		answer: "GitHub issue 和 TestFlight 的反馈是当下推荐的渠道。",
+	},
 ];
