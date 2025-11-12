780b099d80e1bc2b29a7fcb998bb18c3bf17fa1c
ResistanceTo <resistanceto@outlook.com>
2025-11-13 00:26:56 +0800

    修复网络资源无法加载的问题

--- src/config/screenshots.ts
+++ src/config/screenshots.ts
@@ -3,15 +3,15 @@ import type { DeviceScreenshots } from "./types";
 export const screenshots: DeviceScreenshots = {
 	iphone: [
 		{
-			src: "/screenshots/iphone/home.mp4",
+			src: "/screenshots/iphone/HomeList.mp4",
 			type: "video",
 		},
 		{
-			src: "/screenshots/iphone/Video.mp4",
+			src: "/screenshots/iphone/VideoDetail.mp4",
 			type: "video",
 		},
 		{
-			src: "/screenshots/iphone/Comment.mp4",
+			src: "/screenshots/iphone/VideoComment.mp4",
 			type: "video",
 		},
 		"/screenshots/iphone/home.webp",
@@ -19,8 +19,8 @@ export const screenshots: DeviceScreenshots = {
 		"/screenshots/iphone/DynamicImage.webp",
 		"/screenshots/iphone/DynamicVideo.webp",
 		"/screenshots/iphone/search.webp",
-		"/screenshots/iphone/Settings.webp",
-		"/screenshots/iphone/About.webp",
+		"/screenshots/iphone/settings.webp",
+		"/screenshots/iphone/about.webp",
 	],
 	ipad: [
 		"/screenshots/ipad/home.webp",
